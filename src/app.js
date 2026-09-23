9062528e5f8dd4bbba8f9bdd57c2e655cd2acec4
Uparchive <140474627+Uparchive@users.noreply.github.com>
2026-09-22 21:53:39 -0300

    Wire cosmic creation modules

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-import{GenesisEngine}from"./core/engine.js";import{CosmosModule}from"./modules/cosmos.js";import{Astra1}from"./entities/astra-1/astra-1.js";export const genesis=new GenesisEngine();genesis.use(CosmosModule);genesis.create("cosmic.empty-space",{name:"Genesisphere"});genesis.create("cosmic.star",{name:"Genesis-1"});Astra1.create(genesis);globalThis.Genesisphere=genesis;
+import{GenesisEngine}from"./core/engine.js";import{CosmosModule}from"./modules/cosmos.js";import{TemplateModule}from"./templates/template-module.js";import{CosmicPowersModule}from"./powers/cosmic-powers.js";import{Astra1}from"./entities/astra-1/astra-1.js";
+export const genesis=new GenesisEngine();
+genesis.use(CosmosModule).use(TemplateModule).use(CosmicPowersModule);
+const universe=genesis.create("cosmic.empty-space",{name:"Genesisphere"});
+const genesisSystem=genesis.create("cosmic.star-system",{name:"Sistema Genesis",universeId:universe.id});
+const genesisStar=genesis.create("cosmic.star",{name:"Genesis-1",systemId:genesisSystem.id,templateId:"star.g-type"});
+const astraOrbit=genesis.create("cosmic.orbit",{systemId:genesisSystem.id,parentStarId:genesisStar.id,semiMajorAxisAU:1,eccentricity:.0167,periodDays:365.25});
+Astra1.create(genesis,{systemId:genesisSystem.id,parentStarId:genesisStar.id,orbitId:astraOrbit.id});
+globalThis.Genesisphere=genesis;
